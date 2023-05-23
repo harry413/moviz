8733f131f18941eab0ac0f3b94b50994ba59732f
harry413 <sachinpatidar413@gmail.com>
2023-05-23 21:34:10 +0530

    bug fix

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -7,7 +7,7 @@ import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import Cast from './cast/Cast';
 import VideosSection from './videoSection/VideoSection';
-import Similar from './carousels/similar';
+import Similar from './carousels/Similar';
 import Recommendation from './carousels/Recommendation';
 
 
