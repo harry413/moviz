93dfc37342a658a8d9dc61c44ccba6b3283c4236
harry413 <sachinpatidar413@gmail.com>
2023-05-24 15:25:01 +0530

    import correction

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,3 @@
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
 export { default as ContentWrapper } from './contentWrapper/ContentWrapper';
-export { default as Img } from './lazyLoadingImage/img';
