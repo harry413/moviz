83bf9c1114407f58bf27390a65ce6a00de1917f2
harry413 <sachinpatidar413@gmail.com>
2023-05-24 11:27:07 +0530

    bug

--- src/components/contentWrapper/ContentWrapper.jsx
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import './style.scss';
+import "./style.scss";
 
 const ContentWrapper = ({ children }) => {
-    return <div className="ContentWrapper">{children}</div>;
+    return <div className="contentWrapper">{children}</div>;
 };
 
 export default ContentWrapper;
