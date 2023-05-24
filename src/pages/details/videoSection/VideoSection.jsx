8d970716e42fc49a610054b6a9760548c6881a55
harry413 <sachinpatidar413@gmail.com>
2023-05-24 15:26:11 +0530

    import correction

--- src/pages/details/videoSection/VideoSection.jsx
+++ src/pages/details/videoSection/VideoSection.jsx
@@ -5,7 +5,7 @@ import "./videoSection.scss";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import { PlayIcon } from "../PlayIcon";
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
-import Img from "../../../components/lazyLoadingImage/Img";
+import Img from "../../../components/lazyLoadImage/Img";
 
 const VideosSection = ({ data, loading }) => {
     const [show, setShow] = useState(false);
